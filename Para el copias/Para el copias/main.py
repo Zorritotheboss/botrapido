09c1b4193632c701092e92a616fee3106d1386ae
Zorritotheboss <100345937+Zorritotheboss@users.noreply.github.com>
2022-03-13 13:59:53 -0500

    Update main.py

--- Para el copias/Para el copias/main.py
+++ Para el copias/Para el copias/main.py
@@ -230,7 +230,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'AQUI USUARIO DE TELEGRAM AMIND DEL BOT'
+        tl_admin_user = 'ventaorosking'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
